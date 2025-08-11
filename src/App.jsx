19f5b0ca58611a16b3873ec0005f1a73e24f7d75
zayennn <zaayeennn@gmail.com>
2025-08-11 18:24:37 +0700

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const navbarControl = () => {
+      if (window.scrollY > 200) {
+        setIsActive(true);
+      } else {
+        setIsActive(false);
+      }
+      window.addEventListener("scroll", navbarControl);
+      return () => {
+        window.removeEventListener("scroll", navbarControl);
+      };
+    };
+    navbarControl();
+  }, [isActive]);
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckbox = () => {
+    setIsChecked(!isChecked);
+    if (isChecked) {
+      navLinks.classList.remove("active");
+    } else {
+      navLinks.classList.add("active");
+    }
+    setIsActive(!isActive);
+    setIsChecked(!isChecked);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* navbar */}
+      <div className={`navbar__container ${isActive ? "active" : ""}`}>
+        <h1>
+          port<span>folio</span>
+        </h1>
+        <nav className={`nav__links ${isChecked ? "active" : ""}`}>
+          <a href="#" className="nav__link" style={{ "--i": 1 }}>home</a>
+          <a href="#" className="nav__link" style={{ "--i": 2 }}>about</a>
+          <a href="#" className="nav__link" style={{ "--i": 3 }}>skills</a>
+          <a href="#" className="nav__link" style={{ "--i": 4 }}>education & experience</a>
+          <a href="#" className="nav__link" style={{ "--i": 5 }}>projects</a>
+          <a href="#" className="nav__link" style={{ "--i": 6 }}>contact</a>
+        </nav>
+        <label className="hamburger">
+          <input type="checkbox" onChange={handleCheckbox} />
+          <svg viewBox="0 0 32 32">
+            <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22" />
+            <path className="line" d="M7 16 27 16" />
+          </svg>
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
